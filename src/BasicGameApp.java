a94cf075e507c42700e7dc9dc231105257c3762b
AidanSugrue28 <aidan_sugrue28@milton.edu>
2026-05-08 14:55:41 -0400

    blackjack

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -1,8 +1,24 @@
 public class BasicGameApp {
 
-    public Card[]deck;
+    public Card[] deck;
     public Player pl;
     public Dealer dl;
     public boolean gameOn;
 
+
+    public static void main(String[] args) {
+        new BasicGameApp();
+    }
+
+    public BasicGameApp() {
+        System.out.println("welcome to backjack");
+        deck = new Card[52];
+
+        for (int i = 0; i < 13; i++) {
+            deck[i] = new Card(10, i, "diamonds");
+            deck[i].printInfo();
+        }
+
+    }
 }
+
--- src/Card.java
+++ src/Card.java
@@ -3,4 +3,23 @@ public class Card {
     public int value;
     public String name;
     public String suit;
+
+    public Card(int pvalue, int pname, String psuit) {
+        value = pvalue;
+        if (pname ==0){
+            name = "Ace";
+            value = 11;
+        } else if (pname == 1){
+            name = "two";
+            value = 2;
+        }
+        suit = psuit;
+
+        printInfo();
+    }
+
+    public void printInfo() {
+        System.out.println("The " + name + " of " + suit + " is worth "
+                + value + " points.");
+    }
 }
