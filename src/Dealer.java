bd0b2c53d9b77713b0fe7eeaf6328c12ba4dd8ec
AidanSugrue28 <aidan_sugrue28@milton.edu>
2026-05-14 22:26:57 -0400

    blackjack

--- src/Dealer.java
+++ src/Dealer.java
@@ -4,4 +4,27 @@ public class Dealer {
     public int cardTotal;
     public Card[] hand;
     public boolean isBust;
+
+    public Dealer() {
+        isOver16 = false;
+        cardTotal = 0;
+        isBust = false;
+        hand = new Card[10];
+
+        printInfo();
+    }
+
+    public void printInfo() {
+        System.out.println("Dealer Info:");
+        System.out.println("Card Total: " + cardTotal);
+        System.out.println("Is Over 16: " + isOver16);
+        System.out.println("Is Bust: " + isBust);
+
+        System.out.println("Hand:");
+        for (int i = 0; i < hand.length; i++) {
+            if (hand[i] != null) {
+                hand[i].printInfo();
+            }
+        }
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -6,7 +6,27 @@ public class Player {
     public boolean isHit;
     public String name;
 
-    public Player(){
+    public Player() {
+        cardTotal = 0;
+        isBust = false;
+        isHit = false;
+        name = "Player";
+        hand = new Card[10];
 
+        printInfo();
+    }
+
+    public void printInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Card Total: " + cardTotal);
+        System.out.println("Is Bust: " + isBust);
+        System.out.println("Is Hit: " + isHit);
+
+        System.out.println("Hand:");
+        for (int i = 0; i < hand.length; i++) {
+            if (hand[i] != null) {
+                hand[i].printInfo();
+            }
+        }
     }
 }
